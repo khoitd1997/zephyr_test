95efe80abd710395c36b5a83bff0938fac6c4789
khoitd1997 <khoidinhtrinh@gmail.com>
2022-08-02 23:31:02 -0700

    updated script

--- initialize_workspace.sh
+++ initialize_workspace.sh
@@ -5,8 +5,9 @@ set -e
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${script_dir}"
 
+cd app
 west init -l ./app
 west update
 
-source zephyr/zephyr-env.sh
+source ../zephyr/zephyr-env.sh
 west build -p
